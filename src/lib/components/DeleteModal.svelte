56a373711bf4c42cba2346d482f8d7ddba771f87
hansfigo <hambasahaya0303@gmail.com>
2023-10-13 09:31:54 +0700

    fix(DeleteModal.svelte): improve styling and layout of delete modal feat(DeleteModal.svelte): add separate classes for delete and cancel buttons to apply different styles

--- src/lib/components/DeleteModal.svelte
+++ src/lib/components/DeleteModal.svelte
@@ -7,15 +7,26 @@
 </script>
 
 <dialog id="delete_modal" class="modal">
-    <div class="modal-box">
+    <div class="modal-box flex flex-col justify-center items-center">
         <img src="delete-warning.png" alt="">
-        <p class="py-4">Apakah anda yakin menghapus activity “{title}”?</p>
+        <p class="py-4 text-center">Apakah anda yakin menghapus activity <br/>“{title}”?</p>
         <form method="dialog" class="flex gap-4 jb">
-            <button>Batal</button>
-            <button on:click={()=> api.remove(id)}>Hapus</button>
+            <button class="batal-button">Batal</button>
+            <button class="button-delete" on:click={()=> api.remove(id)}>Hapus</button>
         </form>
     </div>
     <form method="dialog" class="modal-backdrop">
         <button>close</button>
     </form>
 </dialog>
+
+<style lang="postcss">
+    .button-delete{
+        @apply bg-red-500 px-8 py-4 rounded-md text-white;
+    }
+
+    .batal-button {
+        @apply bg-gray-300 px-8 py-4 rounded-md text-white;
+
+    }
+</style>
