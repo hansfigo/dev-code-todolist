7df7e5fc9309cc6877c83a121e9bdbb6671646ac
hansfigo <hambasahaya0303@gmail.com>
2023-10-13 21:18:27 +0700

    fix data-cy

--- src/lib/components/DeleteModal.svelte
+++ src/lib/components/DeleteModal.svelte
@@ -5,23 +5,25 @@
 	export let id: string;
 </script>
 
-<dialog id="delete_modal" class="modal">
-	<div  class="modal-box flex flex-col justify-center items-center">
-		<img src="delete-warning.png" alt="" />
-		<p class="py-4 text-center">Apakah anda yakin menghapus activity <br />“{title}”?</p>
-		<form data-cy="modal-delete" method="dialog" class="flex gap-4 jb">
-			<button data-cy="modal-delete-cancel-button" class="batal-button">Batal</button>
-			<button
-				data-cy="modal-delete-confirm-button"
-				class="button-delete"
-				on:click={() => api.remove(id)}>Hapus</button
-			>
+<div>
+	<dialog data-cy="modal-delete"  id="delete_modal" class="modal">
+		<div class="modal-box flex flex-col justify-center items-center">
+			<img src="delete-warning.png" alt="" />
+			<p class="py-4 text-center">Apakah anda yakin menghapus activity <br />“{title}”?</p>
+			<form method="dialog" class="flex gap-4 jb">
+				<button data-cy="modal-delete-cancel-button" class="batal-button">Batal</button>
+				<button
+					data-cy="modal-delete-confirm-button"
+					class="button-delete"
+					on:click={() => api.remove(id)}>Hapus</button
+				>
+			</form>
+		</div>
+		<form method="dialog" class="modal-backdrop">
+			<button>close</button>
 		</form>
-	</div>
-	<form method="dialog" class="modal-backdrop">
-		<button>close</button>
-	</form>
-</dialog>
+	</dialog>
+</div>
 
 <style lang="postcss">
 	.button-delete {
