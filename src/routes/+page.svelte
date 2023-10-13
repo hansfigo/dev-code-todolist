d593c1a07c8139fac6c3a8105b3de5205f5e5828
hansfigo <hambasahaya0303@gmail.com>
2023-10-13 09:42:16 +0700

    fix(+page.svelte): fix indentation and add missing imports for fly, quintOut, and writable feat(+page.svelte): add functionality to show success message when data is added successfully

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,15 @@
 	import ActivityCard from '$lib/components/ActivityCard.svelte';
 	import activityList from '$lib/stores/activity.js';
 	import api from '$lib/utils/api';
-	
+	import { fly } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+	import { writable } from 'svelte/store';
+
 	export let data;
 
-	activityList.set(data.data.data)
+	const isAdded = writable(false);
 
+	activityList.set(data.data.data);
 </script>
 
 <div class="py-8 flex flex-col items-center h-[80vh] relative px-8">
@@ -14,7 +18,12 @@
 		<h1 data-cy="activity-title" class="font-bold text-2xl">Activity</h1>
 		<button
 			data-cy="activity-add-button"
-			on:click={() => api.send()}
+			on:click={async () => {
+				await api.send();
+				isAdded.set(true);
+
+				setTimeout(() => isAdded.set(false), 1200);
+			}}
 			class="bg-main-blue px-5 py-2 font-semibold rounded-full text-white focus:outline-none focus:ring-4 focus:ring-blue-300"
 			>+ Tambah
 		</button>
@@ -25,10 +34,35 @@
 			<img class="md:h-[24rem]" src="activity-empty-state.svg" alt="empty" />
 		</div>
 	{:else}
-		<div class="flex flex-wrap gap-6  items-start w-full container mt-4">
+		<div class="flex flex-wrap gap-6 items-start w-full container mt-4">
 			{#each $activityList as data, i}
-				<ActivityCard created_at={data.created_at} id={data.id} i={i} title={data.title}/>
+				<ActivityCard created_at={data.created_at} id={data.id} {i} title={data.title} />
 			{/each}
 		</div>
 	{/if}
 </div>
+
+{#if $isAdded}
+	<div
+		transition:fly={{ duration: 600, x: 100, y: 500, opacity: 0.5, easing: quintOut }}
+		class="flex justify-center items-center w-full"
+	>
+		<div class="container">
+			<div class="alert alert-success">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="stroke-current shrink-0 h-6 w-6"
+					fill="none"
+					viewBox="0 0 24 24"
+					><path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						strokeWidth="2"
+						d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+					/></svg
+				>
+				<span>Data Berhasil Ditambah</span>
+			</div>
+		</div>
+	</div>
+{/if}
