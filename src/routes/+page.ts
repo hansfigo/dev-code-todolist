bf1cde952442f80ff8cbbc5dbd94862458234b24
hansfigo <hambasahaya0303@gmail.com>
2023-10-13 20:55:58 +0700

    feat(routes): add new route file +page.ts to handle page load with CSR (Client-Side Rendering)

---
+++ src/routes/+page.ts
@@ -0,0 +1,13 @@
+import type { PageLoad } from './$types';
+
+export let csr = true
+export let ssr = false
+export const load = (async () => {
+    const url = 'https://todo.api.devcode.gethired.id/activity-groups?email=claudiofigo0303@gmail.com';
+
+    const res = await fetch(url)
+
+    const data = await res.json()
+
+    return { data };
+}) satisfies PageLoad;
