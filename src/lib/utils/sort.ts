def1e0440f6575cad2944a75f8d179c4a95d9670
hansfigo <hambasahaya0303@gmail.com>
2023-10-13 09:54:24 +0700

    feat(sort.ts): add utility functions for sorting TodoItem array based on different criteria
    - Added `sortByNewest` function to sort items based on the newest date.
    - Added `sortByOldest` function to sort items based on the oldest date.
    - Added `sortByTitleAZ` function to sort items based on the title in ascending order.
    - Added `sortByTitleZA` function to sort items based on the title in descending order.
    - Added `sortByIsCompleted` function to sort items based on the completion status, with completed items appearing first.

---
+++ src/lib/utils/sort.ts
@@ -0,0 +1,29 @@
+import type { TodoItem } from "./activityDetail";
+
+// Fungsi untuk mengurutkan berdasarkan tanggal terbaru
+function sortByNewest(items: TodoItem[]): TodoItem[] {
+    return items.slice().sort((a, b) => b.id - a.id);
+}
+
+// Fungsi untuk mengurutkan berdasarkan tanggal terlama
+function sortByOldest(items: TodoItem[]): TodoItem[] {
+    return items.slice().sort((a, b) => a.id - b.id);
+}
+
+// Fungsi untuk mengurutkan berdasarkan judul A-Z
+function sortByTitleAZ(items: TodoItem[]): TodoItem[] {
+    return items.slice().sort((a, b) => a.title.localeCompare(b.title));
+}
+
+// Fungsi untuk mengurutkan berdasarkan judul Z-A
+function sortByTitleZA(items: TodoItem[]): TodoItem[] {
+    return items.slice().sort((a, b) => b.title.localeCompare(a.title));
+}
+
+// Fungsi untuk mengurutkan berdasarkan status is_active (sudah selesai)
+function sortByIsCompleted(items: TodoItem[]): TodoItem[] {
+    return items.filter(item => item.is_active === 0).concat(items.filter(item => item.is_active === 1));
+}
+
+
+export {sortByIsCompleted, sortByNewest, sortByOldest , sortByTitleAZ, sortByTitleZA}
